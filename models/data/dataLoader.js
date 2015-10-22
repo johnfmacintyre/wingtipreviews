182b32ecf6044559dcdda5d30123ea47cce73fb1
Andrew Liu <aliuy8@gmail.com>
2015-10-22 14:48:39 -0700

    fix dataloader

--- models/data/dataLoader.js
+++ models/data/dataLoader.js
@@ -7,23 +7,23 @@ function DataLoader(documentDBClient, databaseId, collectionId) {
 	this.client = documentDBClient;
 	this.databaseId = databaseId;
 	this.collectionId = collectionId;
-	
+
 	this.database = null;
 	this.collection = null;
 }
 
 DataLoader.prototype = {
-	init: function(callback) {
+	init: function (callback) {
 		var self = this;
-		docdbUtils.getOrCreateDatabase(self.client, self.databaseId, function(err, db) {
+		docdbUtils.getOrCreateDatabase(self.client, self.databaseId, function (err, db) {
 			if (err) {
 				console.log("Error!");
 				console.log(err);
 				callback(err);//something went really wrong!
-			} 
+			}
 			else {
 				self.database = db;
-				docdbUtils.getOrCreateCollection(self.client, self.database._self, self.collectionId, null, function(err, coll) {
+				docdbUtils.getOrCreateCollection(self.client, self.database._self, self.collectionId, null, function (err, coll) {
 					if (err) {
 						console.log("Error!");
 						console.log(err);
@@ -36,16 +36,16 @@ DataLoader.prototype = {
 			}
 		});
 	},
-	
-	checkIfDataExists: function(callback) {
+
+	checkIfDataExists: function (callback) {
 		var self = this;
-		
+
 		var querySpec = {
 			query: 'SELECT * FROM root r',
 			parameters: undefined
 		};
-		
-		self.client.queryDocuments(self.collection._self, querySpec).toArray(function(err, results) {
+
+		self.client.queryDocuments(self.collection._self, querySpec).toArray(function (err, results) {
 			if (err) {
 				console.log("Error!");
 				callback(err);//something went really wrong!!
@@ -54,47 +54,54 @@ DataLoader.prototype = {
 			}
 		});
 	},
-	
-	loadSampleData : function (callback) {
+
+	loadSampleData: function (callback) {
 		var self = this;
+		var data;
 		
-    	var data = fs.readFileSync('./models/data/sampleData.json');   
-    	var jsonDataArray = JSON.parse(data);
-		
-		self.checkIfDataExists(function(err, dataExists) {
-			if(err || dataExists){
+		// Path can change depending on the environment.
+		if (fs.existsSync('./models/data/sampleData.json')) {
+			data = fs.readFileSync('./models/data/sampleData.json', 'utf-8');
+		} else if (fs.existsSync('../models/data/sampleData.json'))   {
+			data = fs.readFileSync('../models/data/sampleData.json', 'utf-8');
+		}
+
+		var jsonDataArray = JSON.parse(data);
+
+		self.checkIfDataExists(function (err, dataExists) {
+			if (err || dataExists) {
 				callback(err);
 				return;
 			}
 			//loading data
 			console.log('loading data...');
-			async.eachSeries(jsonDataArray, 
-				self.addDataObj.bind(self), 
-				function(err){
-					if(err)
-						throw(err);
+			async.eachSeries(jsonDataArray,
+				self.addDataObj.bind(self),
+				function (err) {
+					if (err)
+						throw (err);
 					callback();
 				});
 		})
 	},
-	
-	addDataObj: function(dataObj, callback) {
+
+	addDataObj: function (dataObj, callback) {
 		var self = this;
-	
-		self.client.createDocument(self.collection._self, dataObj, function(err, doc, responseHeaders) {
+
+		self.client.createDocument(self.collection._self, dataObj, function (err, doc, responseHeaders) {
 			if (err) {
 				var status = docdbUtils.checkHttpError(err, responseHeaders);
-				if(status == 0) {
+				if (status == 0) {
 					callback();
 				}
-				else if(status == -1) {
+				else if (status == -1) {
 					console.log("Error!");
 					console.log(err);
 					console.log('Response headers:');
 					console.log(responseHeaders);
 					callback(err);
-				} 
-				else if(status > 0) {
+				}
+				else if (status > 0) {
 					var t = setTimeout(self.addDataObj.bind(self), status, dataObj, callback);
 				}
 				else {
