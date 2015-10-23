fee0f7a75641363aede3426a8b86ba67cc3a4e71
Andrew Liu <aliuy8@gmail.com>
2015-10-23 13:37:57 -0700

    apply new comment schema

--- routes/commentsRoute.js
+++ routes/commentsRoute.js
@@ -9,6 +9,8 @@ CommentsRoute.prototype = {
   writeCommentsByEventId: function (req, res, next) {
     var self = this;
     var eventId = req.params.id;
+    var venueId = req.body.venueId;
+    var artistId = req.body.artistId;
     var user = self.getCurrentUser();
     var comment = req.body.comment;
     
@@ -16,10 +18,13 @@ CommentsRoute.prototype = {
     // var rating = parseInt(req.body.rating);
 
     var commentDoc = {
+      "eventId": eventId,
       "type": "comment",
-      "eventid": eventId,
-      "commentText": comment,
-      "user": user
+      "venueId": venueId,
+      "artistId": artistId,
+      "feedback": comment,
+      "user": user,
+      "test": "test"
       // DEMO: Place the rating in to the DocumentDB document.
       // , "rating": rating
     }
@@ -47,11 +52,7 @@ CommentsRoute.prototype = {
   },
   
   getCurrentUser: function() {
-    return {
-		"id": "561f8d0e-bbae-47a5-b84e-37a1c183b8f6",
-		"firstname": "John",
-		"lastname": "Macintyre"
-    }
+    return "John Macintyre"
   }
 };
 
